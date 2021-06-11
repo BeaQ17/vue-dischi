6099be32794ffd1b648c4086641f850f824611ab
BeaQ17 <beaq235@gmail.com>
2021-06-11 11:11:38 +0200

    axios request

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,21 @@
 const root = new Vue({
     el: "#root",
-    data: {},
+    data: {
+        url: "https://flynn.boolean.careers/exercises/api/array/music",
+        disks: null,
+        error: null
+    },
     methods: {},
     mounted(){
-        
+        axios
+        .get(this.url)
+        .then(response => {
+            console.log(response.data.response);
+            this.disks = response.data.response
+        })
+        .catch (e => {
+            console.error(e);
+            this.error = "Could not access API. Retry later." + e;
+        })
     }
-})
+}) //const root end
